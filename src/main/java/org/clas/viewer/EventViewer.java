f0d82541ecabde169ddb92903d03e08f665e171c
raffaelladevita <devita@ge.infn.it>
2017-01-22 19:38:01 +0100

    first commit

--- src/main/java/org/clas/viewer/EventViewer.java
+++ src/main/java/org/clas/viewer/EventViewer.java
@@ -129,7 +129,7 @@ public class EventViewer implements IDataEventListener, DetectorListener, Action
         splitPanel.setLeftComponent(CLAS12View);
         splitPanel.setRightComponent(CLAS12Canvas);
         JTextPane clas12Text   = new JTextPane();
-        clas12Text.setText("CLAS12\n monitoring plots\n V1.0");
+        clas12Text.setText("CLAS12\n KPP plots\n V1.0");
         clas12Text.setEditable(false);
         StyledDocument styledDoc = clas12Text.getStyledDocument();
         SimpleAttributeSet center = new SimpleAttributeSet();
